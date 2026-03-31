5147a45017da07527fb2e7ba64793ccbf1798444
Hridrita <moonsaha721@gmail.com>
2026-03-31 19:43:51 +0600

    completed banner section

--- src/Components/BannerSection.jsx
+++ src/Components/BannerSection.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { CiPlay1 } from "react-icons/ci";
+import BannerImg from '../assets/banner.png';
 
 const BannerSection = () => {
   return (
-    <div className="container mx-auto">
-      <div>
+    <div>
+      <div className="container mx-auto grid grid-cols-2 mt-20">
+      <div className="space-y-4 mt-10">
         <h3 className="flex items-center gap-2 px-4 py-1 rounded-full font-semibold text-purple-600 bg-[#eef2ff] w-fit border border-purple-100">
           <span className="w-2 h-2 rounded-full bg-purple-600 outline outline-offset-2 outline-purple-300"></span>
           New: AI-Powered Tools Available
         </h3>
 
-        <h1 className="font-bold text-6xl">Supercharge Your Digital Workflow</h1>
+        <h1 className="font-bold text-6xl leading-normal">Supercharge Your Digital Workflow</h1>
 
-        <h3 className="text-[#627382FF]">Access premium AI tools, design assets, templates, and productivity software-all in one place. Start creating faster today. Explore Products
+        <h3 className="text-[#627382FF] leading-relaxed">Access premium AI tools, design assets, templates, and productivity <br></br>software-all in one place. Start creating faster today.<br></br> Explore Products
         </h3>
-
-        <button className="btn bg-linear-to-r from-blue-600 to-purple-600 text-white font-bold rounded-3xl">
+        
+        <div className="flex items-center gap-4">
+          <button className="btn bg-linear-to-r from-blue-600 to-purple-600 text-white font-bold rounded-3xl">
           Explore Products
         </button>
         <button className="btn rounded-full border-2 border-blue-600 bg-transparent px-6 ml-3">
@@ -23,12 +26,38 @@ const BannerSection = () => {
             <CiPlay1 className="text-blue-600" /> Watch Demo
           </div>
         </button>
+
+        </div>
+        
+      </div>
+
+      <div className="justify-self-end">
+        <img src={BannerImg} ></img>
+      </div>
+
+    </div>
+
+    <div className="flex gap-40 justify-center text-center bg-linear-to-l from-[rgb(79,57,246)] to-[rgb(149,20,250)] text-white py-15 px-10 w-full mt-20 ">
+      <div>
+        <h1 className="font-bold text-6xl">50K+</h1>
+        <p>Active Users</p>
+      </div>
+      
+      <div>
+        <h1 className="font-bold text-6xl">200K+</h1>
+        <p>Premium Tools</p>
       </div>
 
       <div>
-        <img sr></img>
+        <h1 className="font-bold text-6xl">4.9</h1>
+        <p>Rating</p>
       </div>
+
     </div>
+    </div>
+    
+
+    
   );
 };
 
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,9 +4,9 @@ import { IoCartOutline } from "react-icons/io5";
 const NavBar = () => {
   return (
     
-      <div className="container mx-auto navbar bg-base-100 shadow-sm flex justify-between">
+      <div className="navbar bg-base-100 shadow-sm flex justify-between">
         <div>
-            <h1 className=" text-3xl font-bold bg-linear-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
+            <h1 className=" text-3xl font-bold bg-linear-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent ml-28">
           DigiTools
         </h1>
         </div>
@@ -22,9 +22,9 @@ const NavBar = () => {
 
 
         <div className="flex gap-3">
-                <h3 className="flex items-center gap-2 font-semibold"> <IoCartOutline />Login</h3>
+                <h3 className="flex items-center gap-2 font-semibold "> <IoCartOutline />Login</h3>
             
-            <button className="btn bg-linear-to-r from-blue-600 to-purple-600 text-white font-bold rounded-3xl">Get Started</button>
+            <button className="btn bg-linear-to-r from-blue-600 to-purple-600 text-white font-bold rounded-3xl mr-26">Get Started</button>
         </div>
       </div>
 
