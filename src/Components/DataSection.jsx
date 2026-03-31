ac23b09bbec85cdb3c4a58914dd0b37c56437c4b
Hridrita <moonsaha721@gmail.com>
2026-03-31 22:58:57 +0600

    created empty cart page

--- src/Components/DataSection.jsx
+++ src/Components/DataSection.jsx
@@ -1,64 +1,69 @@
-import React, { use, useState } from 'react';
-import ProductCard from './ProductCard';
-import SelectedCarts from './SelectedCarts';
+import React, { use, useState } from "react";
+import ProductCard from "./ProductCard";
+import SelectedCarts from "./SelectedCarts";
 
-const DataSection = ({productPromise}) => {
-    console.log(productPromise);
+const DataSection = ({ productPromise }) => {
+  console.log(productPromise);
 
-    const products = use(productPromise)
-    console.log(products);
+  const products = use(productPromise);
+  console.log(products);
 
-    const [selectedType, setSelectedType] = useState("Products");
-    const [selectedCarts, setSelectedcarts] = useState([])
+  const [selectedType, setSelectedType] = useState("Products");
+  const [selectedCarts, setSelectedcarts] = useState([]);
 
-    return (
-        <div>
-            <div className='text-center'>
-                <h1 className='text-4xl font-bold  mt-30 mb-4'>Premium Digital Tools</h1>
-                <p className='text-[#627382FF] leading-relaxed mb-4'>Choose from our curated collection of premium digital products designed<br></br> to boost your productivity and creativity.</p>
-            </div>
+  return (
+    <div>
+      <div className="text-center">
+        <h1 className="text-4xl font-bold  mt-30 mb-4">
+          Premium Digital Tools
+        </h1>
+        <p className="text-[#627382FF] leading-relaxed mb-4">
+          Choose from our curated collection of premium digital products
+          designed<br></br> to boost your productivity and creativity.
+        </p>
+      </div>
 
-            <div className='text-center mb-8'>
-                <button 
-                    onClick={() => setSelectedType("Products")} 
-                    className={`btn rounded-3xl font-bold transition-all ${
-                        selectedType === "Products" 
-                        ? "bg-linear-to-r from-blue-600 to-purple-600 text-white" 
-                        : "bg-gray-200 text-gray-700"
-                    }`}
-                >
-                    Products
-                </button>
+      <div className="text-center mb-8">
+        <button
+          onClick={() => setSelectedType("Products")}
+          className={`btn rounded-3xl font-bold transition-all ${
+            selectedType === "Products"
+              ? "bg-linear-to-r from-blue-600 to-purple-600 text-white"
+              : "bg-gray-200 text-gray-700"
+          }`}
+        >
+          Products
+        </button>
 
-            <button 
-                    onClick={() => setSelectedType("Cart")} 
-                    className={`btn rounded-3xl font-bold transition-all ${
-                        selectedType === "Cart" 
-                        ? "bg-linear-to-r from-blue-600 to-purple-600 text-white" 
-                        : "bg-gray-200 text-gray-700"
-                    }`}
-                >
-                    Cart({selectedCarts.length})
-                </button>
-
-            </div>
-
+        <button
+          onClick={() => setSelectedType("Cart")}
+          className={`btn rounded-3xl font-bold transition-all ${
+            selectedType === "Cart"
+              ? "bg-linear-to-r from-blue-600 to-purple-600 text-white"
+              : "bg-gray-200 text-gray-700"
+          }`}
+        >
+          Cart({selectedCarts.length})
+        </button>
+      </div>
 
 
-            <div className='container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center'>
-                { selectedType === "Products" ? (
-                    products.map(product => (<ProductCard key={product.id} product={product}></ProductCard>))
-
-                ) : (
-                    <SelectedCarts selectedCarts={selectedCarts}></SelectedCarts>
-                )
-                    
-                }
-
-            </div>
-            
-        </div>
-    );
+      <div className={`container mx-auto gap-8 
+      ${selectedType === "Products"
+            ? "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center"
+            : "block"
+        }`}
+      >
+        {selectedType === "Products" ? (
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))
+        ) : (
+          <SelectedCarts selectedCarts={selectedCarts} />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default DataSection;
--- src/Components/SelectedCarts.jsx
+++ src/Components/SelectedCarts.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const SelectedCarts = () => {
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center min-h-75 w-full text-center">
+           
+            <h2 className='font-bold text-2xl text-gray-800 mb-2'>
+                No Carts Selected Yet
+            </h2>
             
+            
+            <p className='text-gray-500 text-lg'>
+                Go to the products tab to select your tools.
+            </p>
         </div>
     );
 };
