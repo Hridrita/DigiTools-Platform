d6078f2cfc181b8563c000494990ae84e9768c18
Hridrita <moonsaha721@gmail.com>
2026-04-01 19:37:47 +0600

    completed pricing cards with resposive design

--- src/Components/PricingCard.jsx
+++ src/Components/PricingCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PricingCard = () => {
   return (
-    <div>
+    <div className="w-400 h-auto shadow-2xl p-30">
       <div className="flex-col justify-center items-center text-center space-y-4">
         <h1 className="text-5xl font-bold">Simple, Transparent Pricing</h1>
         <p className="text-[#627382FF]">
@@ -10,7 +10,7 @@ const PricingCard = () => {
         </p>
       </div>
 
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 mt-8">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 mt-12">
         <div className="card w-96 bg-base-100 shadow-sm border border-gray-100 h-full flex flex-col justify-between">
           <div className="card-body flex flex-col h-full">
             <div className="flex-grow">
@@ -22,85 +22,86 @@ const PricingCard = () => {
                 <span className="text-3xl font-semibold">$0</span>
                 <p className="text-gray-500 text-sm">/Month</p>
               </div>
+              <ul className="mt-8 flex flex-col gap-2 text-xs">
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-4 me-2 inline-block text-success"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                  <span className="text-[#627382FF] font-semibold">
+                    Access to 10 free tools
+                  </span>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-4 me-2 inline-block text-success"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                  <span className="text-[#627382FF] font-semibold">
+                    Basic templates
+                  </span>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-4 me-2 inline-block text-success"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                  <span className="text-[#627382FF] font-semibold">
+                    Community support
+                  </span>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-4 me-2 inline-block text-success"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                  <span className="text-[#627382FF] font-semibold">
+                    1 project per month
+                  </span>
+                </li>
+              </ul>
             </div>
-            <ul className="flex flex-col gap-2 text-xs">
-              <li>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-4 me-2 inline-block text-success"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5 13l4 4L19 7"
-                  />
-                </svg>
-                <span className="text-[#627382FF] font-semibold">
-                  Access to 10 free tools
-                </span>
-              </li>
-              <li>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-4 me-2 inline-block text-success"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5 13l4 4L19 7"
-                  />
-                </svg>
-                <span className="text-[#627382FF] font-semibold">
-                  Basic templates
-                </span>
-              </li>
-              <li>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-4 me-2 inline-block text-success"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5 13l4 4L19 7"
-                  />
-                </svg>
-                <span className="text-[#627382FF] font-semibold">
-                  Community support
-                </span>
-              </li>
-              <li>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-4 me-2 inline-block text-success"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5 13l4 4L19 7"
-                  />
-                </svg>
-                <span className="text-[#627382FF] font-semibold">
-                  1 project per month
-                </span>
-              </li>
-            </ul>
+
             <div className="mt-auto pt-6">
               <button className="btn bg-linear-to-r from-blue-600 to-purple-600 text-white w-full rounded-3xl">
                 Get Started Free
@@ -111,9 +112,11 @@ const PricingCard = () => {
 
         <div className="card w-96 bg-linear-to-r from-[#4F39F6] to-[#9514FA] shadow-sm border border-gray-100 h-full flex flex-col justify-between">
           <div className=" relative card-body flex flex-col h-full">
-            <span className="badge badge-xs badge-warning absolute top-3 self-center-safe">
-              Most Popular
-            </span>
+            <div className="absolute -top-4 left-1/2 -translate-x-1/2">
+              <span className="bg-[#FEF9C3] text-[#854D0E] text-xs font-bold px-4 py-1 rounded-full shadow-md border border-yellow-200 whitespace-nowrap">
+                Most Popular
+              </span>
+            </div>
             <div className="flex-grow">
               <h2 className="text-3xl text-white font-bold mb-2">Pro</h2>
               <p className="text-white mb-5">Best for professionals</p>
@@ -250,8 +253,7 @@ const PricingCard = () => {
           <div className="card-body flex flex-col h-full">
             <div className="flex-grow">
               <h2 className="text-3xl font-bold mb-2">Enterprise </h2>
-              <p className="text-[#627382FF] mb-5">For teams and businesses
-              </p>
+              <p className="text-[#627382FF] mb-5">For teams and businesses</p>
               <div className="flex items-baseline">
                 <span className="text-3xl font-semibold">$99</span>
                 <p className="text-gray-500 text-sm">/Month</p>
